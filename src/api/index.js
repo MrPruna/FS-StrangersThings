3f631df103dbbf7025dc9f747d7d00cd874bdb18
Pruna <CaroPruna@pm.me>
2023-02-17 22:00:00 +0530

    New Source Files

---
+++ src/api/index.js
@@ -0,0 +1,38 @@
+export const BASE_URL = 'https://strangers-things.herokuapp.com/api/2211-FTB-ET-WEB-AM'
+
+export const fetchAllPosts = async () => {
+    try {
+        const response = await fetch (`${BASE_URL}/posts`)
+        const result = await response.json()
+
+        console.log(result)
+        if (result.error) throw result.error
+        // return result.data.posts
+    } catch(err) {
+        console.error('Error fetching post data: ', err)
+    }
+}
+
+export const registerUser = async () => {
+
+    const response = await fetch(`${BASE_URL}/users/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+        body: JSON.stringify({ user: {
+            username: `${username}`,
+            password: `${password}`
+        }
+        })    
+    })
+    const result = await response.json()
+    console.log(success)
+    localStorage.setItem('token', result.data.token)
+    console.log(localStorage)
+    return
+}
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import { createRoot } from 'react-dom/client';
+import { Header, Login, Profile, Posts, Register, NewPost } from './components'
+import { BrowserRouter as Router,
+    useNavigate,
+    Routes,
+    Route,
+    Link } from 'react-router-dom'
+
+const App = () => {
+    const [hasToken, setHasToken] = useState(false)
+    const navigate = useNavigate()
+
+    const tokenChecker = () => {
+        localStorage.getItem('token') ? setHasToken(true) : setHasToken(false)
+    }
+
+    useEffect(() => {
+        localStorage.getItem('token')
+        tokenChecker()      
+    }, [])
+
+    return(
+            <div>
+                <Header />
+                    <Routes>
+                        {/* <Route path='/Search' element={<Search /> }></Route> */}
+                    <Route exact path='/' element={<Posts />}></Route>
+                    <Route path='/home' element={<Posts />}></Route>
+                    <Route path ='/login' element={<Login />}></Route>
+                    <Route path='/register' element={<Register />}></Route>
+                    <Route path='/create_post' element={<NewPost />}></Route>
+                    <Route path='/profile' element={<Profile />}></Route>
+                </Routes>
+            </div>
+    )
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+    <Router>
+        <App />
+    </Router>);
